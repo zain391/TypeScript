cce2ea2993aa1a480d6215709cbd7b33b7473bec
zain391 <zainqamar391@gmail.com>
2022-01-29 21:57:21 +0500

    working on TS first exercise
    working on TS first exercise

---
+++ hello.js
@@ -0,0 +1,6 @@
+// console.log("i am zain");
+function muneeb(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() +
+    );
+}
+muneeb("Maddison", new Date());
---
+++ hello.ts
@@ -0,0 +1,7 @@
+// console.log("i am zain");
+
+function muneeb1(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString())
+   }
+   muneeb1("Maddison", new Date());
+   
