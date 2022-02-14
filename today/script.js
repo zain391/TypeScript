2a8581d1bf56b9015877aa401d11264944b0752d
zain391 <zainqamar391@gmail.com>
2022-02-15 00:47:28 +0500

    working on the arrays
    working on the arrays

--- today/script.js
+++ today/script.js
@@ -1,2 +1,14 @@
-document.write("i am heading one")
-document.write(34);
+// document.write("i am heading one")
+// document.write(34);
+// document.write("<br> enter your age ");
+// let box=prompt("enter your age");
+// document.write(box);
+// console.log(box);
+// alert("i am an alert");
+// let friend1=prompt("enter the name 1st of your best freind");
+// let friend2=prompt("enter the name 2nd of your best freind");
+// let friend3=prompt("enter the name  3rd of your best freind");
+// let friend4=prompt("enter the name  4th of your best freind");
+// let friend5=prompt("enter the name  5th of your best freind");
+// let arr=[friend1,friend2,friend3,friend4,friend5];
+// console.log(arr);
