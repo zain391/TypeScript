001e64fa7afb6b886c9ed8d4b5a9f19e13dc5640
zain391 <zainqamar391@gmail.com>
2022-03-20 01:27:54 +0500

    added data to js file
    added data to js file

---
+++ index.js
@@ -0,0 +1,17 @@
+// we are going to learn about the js as well as the ts
+// firs twe will do the variables and data types
+var num = 45;
+console.log(typeof (num));
+var num1 = 45.56;
+console.log(typeof (num));
+console.log(num1);
+var str1 = "i am a string and also have data type string";
+console.log(str1);
+// alert("i am an alert");
+// let alert1:string=alert("i am an alert");
+// let alert2:string=alert("i am an alert");
+// now we will learn about the functions in the js 
+function name1(name) {
+    console.log("name");
+}
+name1("zain");
