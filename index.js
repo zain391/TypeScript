0fc31d97b814f635784e3be0b9d790cd1772a518
zain391 <zainqamar391@gmail.com>
2022-03-20 01:04:21 +0500

    first program about the number data type in the ts
    first program about the number data type in the ts

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("i am the first program ");
+// we are going to learn about the js as well as the ts
+// firs twe will do the variables and data types
+var num = 45;
+console.log(typeof (num));
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("i am the first program ");
+// we are going to learn about the js as well as the ts
+// firs twe will do the variables and data types
+const num:number=45;
+console.log(typeof(num));
+
