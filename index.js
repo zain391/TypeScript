ff6e6ec207c0d177cb6fc5f107967c15653fe456
zain391 <zainqamar391@gmail.com>
2022-03-20 01:36:21 +0500

    added text to js file
    added text to js file

---
+++ index.js
@@ -0,0 +1,18 @@
+// we are going to learn about the js as well as the ts
+// firs twe will do the variables and data types
+var num = 45;
+console.log(typeof (num));
+var num1 = 45.56;
+console.log(typeof (num));
+console.log(num1);
+var str1 = "i am a string and also have data type string";
+console.log(str1);
+// alert("i am an alert");
+// let alert1:string=alert("i am an alert");
+// let alert2:string=alert("i am an alert");
+// now we will learn about the functions in the js 
+function name1(name, age) {
+    console.log("i am ".concat(name, " i am ").concat(age, " year old"));
+    console.log("i am" + name + age);
+}
+name1(20, "zain");
