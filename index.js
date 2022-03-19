bcc1daa1cb476f81f5ea3b74c812fac3ec43c1db
zain391 <zainqamar391@gmail.com>
2022-03-20 01:05:35 +0500

    second program on the number data type
    second program on the number data type

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 // firs twe will do the variables and data types
 var num = 45;
 console.log(typeof (num));
+var num1 = 45.56;
+console.log(typeof (num));
+console.log(num1);
--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@
 // firs twe will do the variables and data types
 const num:number=45;
 console.log(typeof(num));
+const num1:number=45.56;
+console.log(typeof(num));
+console.log(num1);
+
 
