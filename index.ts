7d812c1a2b8568ada18bf9661d57b09fd3e36c9a
zain391 <zainqamar391@gmail.com>
2022-03-20 22:19:09 +0500

    we are good to converting the milliseconds into days and hours and seconds
    we are good to converting the milliseconds into days and hours and in seconds

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ let dd:Date=new Date();
 let required:Date=new Date("january 21, 2025");
 let dd2=dd.getTime();
 let dd3=required.getTime();
-let milSec:any=dd2-dd3;
-const diff:number=milSec/(1000*60*60*24)
+let milSec:any=dd3-dd2;
+let diff:number=milSec/(1000*60);
+diff=Math.ceil(diff);
 console.log(diff);
 
 // let milSec:Number=(required-dd);
