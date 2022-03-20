423911cbb6e4770e5bb7e784a058c225d0bc1e74
zain391 <zainqamar391@gmail.com>
2022-03-20 21:36:14 +0500

    Playing with dates in the Typescript
    Playing with dates in the Typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
 // now we are going to learn about the dates in the typescript
 let date1:Date=new Date();
 console.log(date1);
-
+console.log(date1.toString());
+function display(d:Date){
+    console.log("getFullYear",d.getFullYear());
+    console.log("getTiem stamp",d.getTime());
+    console.log("getDay",d.getDay());
+    console.log("getHours",d.getHours());
+    console.log("getMinutes",d.getMinutes());
+    console.log("getSeconds",d.getSeconds());
+    console.log("getMilliseconds",d.getMilliseconds());
+    
+}
+display(new Date());
 
 
 
