d316b66f72fd5aaba5755e0939cd77528bb768dc
zain391 <zainqamar391@gmail.com>
2022-03-20 21:52:43 +0500

    fixed error in the doomsday example
    fixed error in the doomsday example

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// now we are going to learn about the dates in the typescript
-var date1 = new Date();
-console.log(date1);
-console.log(date1.toString());
-function display(d) {
-    console.log("getFullYear", d.getFullYear());
-    console.log("getTiem stamp", d.getTime());
-    console.log("getDay", d.getDay());
-    console.log("getHours", d.getHours());
-    console.log("getMinutes", d.getMinutes());
-    console.log("getSeconds", d.getSeconds());
-    console.log("getMilliseconds", d.getMilliseconds());
-}
-display(new Date());
 var dd = new Date();
 var required = new Date("january 21, 2025");
-console.log(dd);
-console.log(required);
-console.log(typeof (required));
+var dd2 = dd.getTime();
+var dd3 = required.getTime();
+var milSec = dd2 - dd3;
+console.log(milSec);
+console.log(typeof (dd2));
+console.log(typeof (dd3));
 // let milSec:Number=(required-dd);
