d94d010f69c9c0cb867e2ab26ad1af4197b2db12
zain391 <zainqamar391@gmail.com>
2022-03-20 22:09:47 +0500

    got an error in logic
    got an error in logic

--- index.ts
+++ index.ts
@@ -3,8 +3,7 @@ let required:Date=new Date("january 21, 2025");
 let dd2=dd.getTime();
 let dd3=required.getTime();
 let milSec:any=dd2-dd3;
-console.log(milSec);
-console.log(typeof(dd2));
-console.log(typeof(dd3));
+const diff:number=milSec/(1000*60*60*24)
+console.log(diff);
 
 // let milSec:Number=(required-dd);
