78f77744294d11ea4eb1d79730ea98f027633b48
zain391 <zainqamar391@gmail.com>
2022-03-20 20:22:22 +0500

    working with number arrays and also slicing and splicing in array
    working with number arrays and also slicing and splicing in array

--- index.ts
+++ index.ts
@@ -52,8 +52,12 @@ for (var i = 0; i < arr.length; i++) {
 }
 console.log(arr);
 // converting array to string
-let arr2:number[]=[0,1,2,3,4,5,6,7,8,9];
-console.log(arr2.toString());
+let arr3:number[]=[0,1,2,3,4,5,6,7,8,9];
+console.log(arr3.slice(0,3));
+arr3=arr3.splice(3,1);
+console.log("i am array arr3 with splicing",arr3);
+
+console.log(arr3.toString());
 
 
 
