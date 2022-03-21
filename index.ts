a4c75b6a5c9dc6eb3b2f82cb325eca9439923e66
zain391 <zainqamar391@gmail.com>
2022-03-21 21:54:33 +0500

    for in loop for object key values
    for in loop for object key values

--- index.ts
+++ index.ts
@@ -12,4 +12,39 @@ const funfun1=funfun([1,2,3,4,5,]);
 const funfun2=funfun(['1','2','3','4','5']);
 // so this is how we can use the generics
 
+// now we are going to learn about the for in loops in js
+// for in loop is used for the objects in the js
+interface obj{
+    name:string,
+    age:number,
+    isMale:boolean,
+    birthMark:null,
+}
+let objOne:obj={
+    name:"zain",
+    age:20,
+    isMale:true,
+    birthMark:null,
+}
+// now we are going to use the for in loop to read the values in objects
+for(let x in objOne){
+    console.log(objOne[x]);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
