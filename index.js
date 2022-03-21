106af9a0dfc9d7ecd96b2ae3557a3d944bb28da8
zain391 <zainqamar391@gmail.com>
2022-03-21 16:39:32 +0500

    using the logical operators
    using the logical operators

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var d = new Date();
-var mySecDate2 = d.getSeconds();
-var dd = new Date('january 20,2021');
-var mySecDate1 = dd.getSeconds();
-console.log(mySecDate1 - mySecDate1);
 // now we are going to learn about the if else and switch
 // if color is green and height greater than 5.10 and age is greater than 20 
 // then show you are hulk else show you are not hulk 
@@ -12,19 +7,10 @@ var color = prompt("enter  color");
 var height = prompt("enter height");
 var strAge = parseInt(age);
 var strHei = parseInt(height);
-if (strAge >= 20) {
-    if (color == "green") {
-        if (strHei >= 5.10) {
-            console.log("you are hulk");
-        }
-        else {
-            console.log("you are not hulk because you have short height");
-        }
-    }
-    else {
-        console.log("you are not hulk because you have other color");
-    }
+// now we are going to do this by using the logical operators
+if (strAge >= 20 && color == 'green' && strHei > 5.10) {
+    console.log("you are hulk ");
 }
 else {
-    console.log("you are not hulk because you are not a teen");
+    console.log("you are not hulk ");
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-let d:Date=new Date();
-let mySecDate2=d.getSeconds();
-let dd=new Date('january 20,2021');
-let mySecDate1=dd.getSeconds();
-console.log(mySecDate1-mySecDate1);
 // now we are going to learn about the if else and switch
 // if color is green and height greater than 5.10 and age is greater than 20 
 // then show you are hulk else show you are not hulk 
@@ -14,22 +9,12 @@ const color=prompt("enter  color");
 const height=prompt("enter height");
 let strAge=parseInt(age);
 let strHei=parseInt(height);
-if(strAge>=20){
-    if(color=="green"){
-        if(strHei>=5.10){
-            console.log("you are hulk");
-            
-        }else{
-            console.log("you are not hulk because you have short height");
-            
-        }
-    }
-    else{
-        console.log("you are not hulk because you have other color");
-            
-    }
-}else{
-    console.log("you are not hulk because you are not a teen");
-            
-}
 
+// now we are going to do this by using the logical operators
+if(strAge>20 && color=='green' && strHei>5.10){
+    console.log("you are hulk ");
+    
+}else{
+    console.log("you are not hulk ");
+    
+}
