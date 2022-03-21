9443f1c0a9edfc2a5abc66377ce7331f3b2bd3b0
zain391 <zainqamar391@gmail.com>
2022-03-21 23:46:46 +0500

    added or loop text vs foreach loop in the js
    added or loop text vs foreach loop in the js

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-// create an array of fruits and display a message 
-type arr=Array<string>;
-let fruits:arr=['mango','orange','avacado','watemallons'];
-fruits.forEach((fruitName)=>{
-        console.log('i want to eat a ',fruitName);
-})
+// now we are going to learn about the higher order arrays functions
+// like the fillter and foreach loop and maps also redce and sort
+// first we are going to do the foreach 
+type arr=Array<number>;
+let arr1:arr=[1,2,3,43,,6,7,7,8,8,899,0,0];
+// we want to log all the numberes
+for(let i:number=0; i<arr1.length; i++){
+        console.log(arr1[i]);
+}
+// above line will do the work but we will do this byusing the foreach method
