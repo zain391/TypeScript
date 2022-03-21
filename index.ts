ead4c7802bc937af5aff743657253c1c7336f7d6
zain391 <zainqamar391@gmail.com>
2022-03-21 18:18:29 +0500

    Generics in Arrays
    Generics in Arrays

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-// now we are going to learn about the 
+// now we are going to learn about the Generics in typescript
+// this is an array of generics 
+type arr=Array<string>;
+let arr1:arr=['zain'];
+let arr2:string[]=['zain'];
