0aaad6f80400e40f2db294ac60b76c5c1c6a2018
zain391 <zainqamar391@gmail.com>
2022-03-22 22:02:02 +0500

    how to use map with other map , so map is a powerful tool
    how to use map with other map , so map is a powerful tool

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 // we were working on the higher order arrays loops 
 // now we are going to do the map 
-type arr=Array<string>;
-let arrOfStr=['zain','usama','umer','Ali','Ahmed'];
+type arr1=Array<string>;
+type arr2=Array<number>;
+let arrOfStr:arr1=['zain','usama','umer','Ali','Ahmed'];
 arrOfStr.map(function (item){
     return item;
 })
+let arrOfNum:arr2=[34,5,6,7,87,87,98,89,67,83,45,54];
+// now we are going to use the map loop on the num array 
+const result2=arrOfNum.map(item=>console.log(item));
+const result3=arrOfNum.map(item=>Math.sqrt(item)).map(item=>item*34);
+// so in this way we can use map multiple times so map is a powerfull tool 
 
 
