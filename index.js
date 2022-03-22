1e0742b2acfa6dd618126ec74bb8fe57aa112f8f
zain391 <zainqamar391@gmail.com>
2022-03-22 21:50:57 +0500

    learned how to store return value of an anonymous function in a variable
    learned how to store return value of an anonymous function in a variable

---
+++ index.js
@@ -0,0 +1,6 @@
+var arrOfStr = ['zain', 'usama', 'umer', 'Ali', 'Ahmed'];
+const result=arrOfStr.map(function (item) {
+    return item;
+});
+console.log(result);
+
---
+++ index.ts
@@ -0,0 +1,9 @@
+// we were working on the higher order arrays loops 
+// now we are going to do the map 
+type arr=Array<string>;
+let arrOfStr=['zain','usama','umer','Ali','Ahmed'];
+arrOfStr.map(function (item){
+    return item;
+})
+
+
