b83dee89623572a438d852351ca5034eb07ad4c2
zain391 <zainqamar391@gmail.com>
2022-03-22 22:19:18 +0500

    hiding image from user
    hiding image from user

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var arrOfStr = ['zain', 'usama', 'umer', 'Ali', 'Ahmed'];
-arrOfStr.map(function (item) {
-    return item;
-});
-var arrOfNum = [34, 5, 6, 7, 87, 87, 98, 89, 67, 83, 45, 54];
-// now we are going to use the map loop on the num array 
-var result2 = arrOfNum.map(function (item) { return console.log(item); });
-var result3 = arrOfNum.map(function (item) { return Math.sqrt(item); }).map(function (item) { return item * 34; });
-// so in this way we can use map multiple times so map is a powerfull tool 
+const btn=document.getElementById('btn');
+btn.addEventListener('click',function (){
+    const img=document.getElementById('img');
+    img.className='hidden';
+})
