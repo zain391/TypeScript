50ca109b011ab29ca3a4d6a21410867edbb7f2f0
zain391 <zainqamar391@gmail.com>
2022-03-24 00:46:50 +0500

    Pakistan Geography in term of objects in typescript
    Pakistan Geography in term of objects in typescript

--- index.ts
+++ index.ts
@@ -23,3 +23,27 @@ let myself2:objects={
     age:20,
 }
 greet2(myself2);
+// in some of our cases we do not want to use all the properties of the objects so in that particular case we add the Question mark at the end of the property 
+interface Qusetionmark{
+    east:string,
+    nortwest?:string,
+    west?:string,
+    northEast?:string,
+}
+const pak1:Qusetionmark={
+    east:'India'
+}
+// as we can see know the typescript is not giving us error and still we are not using all the properties of the object 
+const pak2:Qusetionmark={
+        nortwest:'Afghanistan'
+}
+// now we can see that we did not add the ? with tht east so the typescript is gu=iving us the error that we have to use it 
+const pak3:Qusetionmark={
+    east:'India',
+    nortwest:"Afghanistan",
+    west:'Iran',
+    northEast:'northEast',
+}
+console.log(pak1);
+console.log(pak2);
+console.log(pak3);
