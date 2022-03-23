73612395400c170f0b90138cd26823dec972f3f7
zain391 <zainqamar391@gmail.com>
2022-03-24 00:26:12 +0500

    so this is how we declare objects in the typescript and use it JavaScript
    so this is how we declare objects in the typescript and use it JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function checkForSelection(selecID) {
-    var target = document.getElementById(selecID);
-    var num = 0;
-    if (target.selectedIndex === num) {
-        alert("Please select a state.");
-        var random = false;
-        return random;
-    }
+// now we are going to study objects in details
+var myself = {
+    name: 'zain',
+    age: 30
+};
+function greet(person) {
+    console.log(typeof person);
 }
+greet(myself);
+// now as we have created the interface for the object we can use it 
+function greet2(person1) {
+    return person1.name;
+}
+var myself2 = {
+    name: 'zain2',
+    age: 20
+};
+greet2(myself2);
--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
-function checkForSelection(selecID) {
-     var target:any = document.getElementById(selecID);
-     let num:number=0;
-     if (target.selectedIndex === num) {
-     alert("Please select a state.");
-     let random:boolean=false;
-     return random;
-     }
-     }
+// now we are going to study objects in details
+const myself={
+    name:'zain',
+    age:30,
+}
+function greet(person:{name:string, age:number}){
+    console.log(typeof person);
     
+}
+greet(myself);
+// the methode above is not efficent as it make the code more complex as is if we want to add more properties in the object  the code in the one line we will be increasing gradually 
+// so we  made interfaces for the objects in the typescript 
+interface objects{
+    name :string,
+    age:number,
+}
+// now as we have created the interface for the object we can use it 
+function greet2(person1:objects){
+    return person1.name;
+}
+let myself2:objects={
+    name:'zain2',
+    age:20,
+}
+greet2(myself2);
