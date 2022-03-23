79dcde248b504c82a542738c7ec023e28257a678
zain391 <zainqamar391@gmail.com>
2022-03-23 21:13:38 +0500

    this how the methods are added in the js object
    this how the methods are added in the js object

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// now we will try do some hacker rank problem solving 
-let arr=[5,2,3,6,6,5];
-let num=0;
-for(let i=0; i<arr.length; i++){
-    if(arr[i]<=arr[i+1]){
-        console.log(arr[i+1]);
-    }else{
-        console.log(arr[i]);
-    }
-}
+// whenever we add a function as a object property it became the method  of that function 
+var plan1 = {
+     name: "Basic",
+    price: 3.99,
+    space: 100,
+    transfer: 1000,
+    pages: 10,
+    discountMonths: [6, 7],
+    calcAnnual: function(percentIfDisc) {
+     var bestPrice = plan1.price;
+     var currDate = new Date();
+     var thisMo = currDate.getMonth();
+     for (var i = 0; i < plan1.discountMonths.length; i++) {
+     if (plan1.discountMonths[i] === thisMo) {
+     bestPrice = plan1.price * percentIfDisc;
+     break;
+     }
+     }
+     return bestPrice * 12;
+     }
+     };
+    //  the calcAnnual is the method of the object plan1 
+    // the function is returning the value after disscount 
