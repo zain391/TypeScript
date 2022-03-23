bf18e03e593ef6afdf496f5eb414b12b09b48c2b
zain391 <zainqamar391@gmail.com>
2022-03-23 21:01:07 +0500

    we have done a problem of hacker rank
    we have done a problem of hacker rank

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-let objPlan2={
-    name:'proffesional',
-    price:20,
-    space:500,
-    transfer:500,
-    pages:10,
-    discountMonths:'Jul,Aug,Dec',
+// now we will try do some hacker rank problem solving 
+let arr=[5,2,3,6,6,5];
+let num=0;
+for(let i=0; i<arr.length; i++){
+    if(arr[i]<=arr[i+1]){
+        console.log(arr[i+1]);
+    }else{
+        console.log(arr[i]);
+    }
 }
-function calcAnnual() {
-    var bestPrice = objPlan2.price;
-    var currDate = new Date();
-    var thisMo = currDate.getMonth();
-    for (var i = 0; i < objPlan2.discountMonths.length; i++) {
-    if (objPlan2.discountMonths[i] === thisMo) {
-    bestPrice = objPlan2.price * .8;
-    break;
-    }
-    }
-    return bestPrice * 12;
-    }
