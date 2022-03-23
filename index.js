0ae9d93246b82979f0f648030be3d8c1d87aca54
zain391 <zainqamar391@gmail.com>
2022-03-24 02:08:26 +0500

    delete property from the object , also how to check if a property in the object do exit or not
    delete property from the object , also how to check if a property in the object do exit or not

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const province={
+const pakistan={
     howMany:4,
     first:'Punjab',
     second:'KPK',
@@ -10,8 +10,24 @@ const province={
         console.log("i am name1=this.first; " ,name1);
     }
 }
-console.log('1',province);
+console.log('1',pakistan);
 // the line below will give us the function definition but not the result  
-console.log('2',province.readFirstPro);
+console.log('2',pakistan.readFirstPro);
 // now we want to only display  the readFirst value
-console.log('3',province.readFirstPro());
+console.log('3',pakistan.readFirstPro());
+// and we can also add the property in the object 
+//firs we all add the property in the object 
+pakistan.Pm='Imran Khan';
+console.log("i was not in the object ",pakistan.Pm);
+// we can also check if a property in the object exit or not
+const checkPoint='Pm' in pakistan;
+console.log("it should either be true or false ",checkPoint);
+// we can also delete the propety of the object 
+
+delete pakistan.Pm;
+console.log("i was deleted from the object ",pakistan.Pm);
+
+
+
+
+
