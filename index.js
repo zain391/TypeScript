8dbeec3c55c0fc219b45913047be7e25f48cd867
zain391 <zainqamar391@gmail.com>
2022-03-23 18:36:53 +0500

    created the interface for the object in typescript
    created the interface for the object in typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log(document.childNodes[1].childNodes[0].childNodes[0]);
-console.log(document.childNodes[1].childNodes[0].childNodes[1]);
-console.log(document.childNodes[1].childNodes[0].childNodes[2]);
-console.log(document.childNodes[1].childNodes[0].childNodes[3]);
-console.log(document.childNodes[1].childNodes[0].childNodes[4]);
-console.log(document.childNodes[1].childNodes[0].childNodes[5]);
-console.log(document.childNodes[1].childNodes[0].childNodes[6]);
-console.log(document.childNodes[1].childNodes[0].childNodes[7]);
