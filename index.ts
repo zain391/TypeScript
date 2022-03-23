8b53dd6a351992030ff95d04a63c398163071f24
zain391 <zainqamar391@gmail.com>
2022-03-23 18:37:14 +0500

    created the interface for the object in typescript
    created the interface for the object in typescript

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
 // now we are going to learn about the objects in the ts
 // MERN MEAN MEVN 
+interface obj{
+    name:string,
+    price:number,
+    space:number,
+    transfer:number,
+    pages:number,
+    discountMonths:number,
+}
+
+
+
