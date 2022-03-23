b4c991a8c6cf4cff10140adc37c0254857eeaa4e
zain391 <zainqamar391@gmail.com>
2022-03-23 18:50:10 +0500

    added objects in the javascript
    added objects in the javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+var objPlan = {
+    name: 'basic',
+    price: 10,
+    space: 10,
+    transfer: 200,
+    pages: 7,
+    discountMonths: 'Jul,Aug'
+};
+var objPlan2 = {
+    name: 'proffesional',
+    price: 20,
+    space: 500,
+    transfer: 500,
+    pages: 10,
+    discountMonths: 'Jul,Aug,Dec'
+};
--- index.ts
+++ index.ts
@@ -6,8 +6,21 @@ interface obj{
     space:number,
     transfer:number,
     pages:number,
-    discountMonths:number,
+    discountMonths:string,
 }
-
-
-
+let objPlan:obj={
+    name:'basic',
+    price:10,
+    space:10,
+    transfer:200,
+    pages:7,
+    discountMonths:'Jul,Aug',
+}
+let objPlan2:obj={
+    name:'proffesional',
+    price:20,
+    space:500,
+    transfer:500,
+    pages:10,
+    discountMonths:'Jul,Aug,Dec',
+}
