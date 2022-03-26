bb3de6c69f6943da528014fc72292af973746020
zain391 <zainqamar391@gmail.com>
2022-03-26 20:59:52 +0500

    we are good to go with promises in the js
    we are good to go with promises in the js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// now we are going to learn about the call back functions
-let studentsFirstArr=[
-    {name1:'zain',stdId:123},
-    {name1:'umer',stdId:456},
-    {name1:'Ali',stdId:789}
-];
-
-const enrollStudents=function (stdPara){
-    console.log('std at first enroll ',studentsFirstArr);
-    setTimeout(function () {
-        // stdPara.push(studentsFirstArr);
-        studentsFirstArr.push(stdPara);
-        console.log('std after at first enroll ',studentsFirstArr);
-    }, 5000);
+// now we are going to learn about the promises in the js
+const fun1=function (){
+    return new Promise(function (resolve,reject){
+        setTimeout(() => {
+            const val=true;
+            if(!val){
+                console.log("you have come to meet me ");
+                resolve();
+            }else{
+                console.log('you did not came to see me');
+                reject();
+            }
+        }, 3000);
+    });
 }
-const getStudents=function (stdPara2){
-    setTimeout(function () {
-        studentsFirstArr.forEach(function (items){
-            console.log(items.name1);
-        })
-            console.log("these are the std whoare enrolled before and after both",stdPara2);
-    }, 3000);
-}
-const newStd={name1:"Qamer",stdId:000};
-// console.log(enrollStudents);
-enrollStudents(newStd);
-getStudents(studentsFirstArr);
+fun1().then(
+    function (){
+        document.write("as you have resolved your promise now we will go out to have a drink")
+    }
+).catch(function (){
+    document.write("you rejected your promise now i will no longer talk to you ")
+})
