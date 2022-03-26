60269aecfa06a49218134848c8aab1f3e4c15350
zain391 <zainqamar391@gmail.com>
2022-03-26 20:33:14 +0500

    we are good to go with the callback functions in the js
    we are good to go with the callback functions in the js

--- index.js
+++ index.js
@@ -1,57 +1,27 @@
-// // again browser object model
-// const ran=window.location;
-// const firstp=ran.href;
-// const secondp=ran.hash;
-// const threep=ran.host;
-// const fourp=ran.pathname;
-// const fivep=ran.port;
-// console.log('port ',fivep);
-// console.log('firstp',firstp);
-// console.log('secondp hash',secondp);
-// console.log('three host',threep);
-// console.log('fourp pathname',fourp);
+// now we are going to learn about the call back functions
+let studentsFirstArr=[
+    {name1:'zain',stdId:123},
+    {name1:'umer',stdId:456},
+    {name1:'Ali',stdId:789}
+];
 
-// now we are going to learn about the exception handling
-// console.log('it should be in the console 1');
-// function playingWithTryCatch1(){
-//     const playingWithTryCatch2='i am an alert with wrong speelings';
-//     alet(playingWithTryCatch2)
-
-// }
-// playingWithTryCatch1();
-// console.log('it should be in the console 2');
-
-
-
-
-
-
-
-
-
-
-
-
-function playingWithTryCatch1(){
-    try{
-        const playingWithTryCatch2='i am an alert with wrong speelings';
-    alet(playingWithTryCatch2)
-    }catch(err){
-       console.log(err);
-    }
-
+const enrollStudents=function (stdPara){
+    console.log('std at first enroll ',studentsFirstArr);
+    setTimeout(function () {
+        // stdPara.push(studentsFirstArr);
+        studentsFirstArr.push(stdPara);
+        console.log('std after at first enroll ',studentsFirstArr);
+    }, 5000);
 }
-playingWithTryCatch1();
-console.log('i am after try catch');
-const val=prompt("enter a value between 5 to 10");
-if(val<5 || val>10){
-    throw('plz enter a valid value');
+const getStudents=function (stdPara2){
+    setTimeout(function () {
+        studentsFirstArr.forEach(function (items){
+            console.log(items.name1);
+        })
+            console.log("these are the std whoare enrolled before and after both",stdPara2);
+    }, 3000);
 }
-
-
-
-
-
-
-
-
+const newStd={name1:"Qamer",stdId:000};
+// console.log(enrollStudents);
+enrollStudents(newStd);
+getStudents(studentsFirstArr);
