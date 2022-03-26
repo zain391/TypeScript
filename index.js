4e5d5da161085372e76a6b737e1aebbbab017856
zain391 <zainqamar391@gmail.com>
2022-03-26 15:26:04 +0500

    BOM browser object model
    BOM browser object model

--- index.js
+++ index.js
@@ -1,70 +1,57 @@
-var plan1 = {
-    name: "Basic",
-    price: 3.99,
-    space: 100,
-    transfer: 1000,
-    pages: 10,
-    discountMonths: [2,6, 7],
-    // now we are going to create a method for the object an then we will emmbed it in the object for its use
-    calAnnual:function (){
-        let bestPrice=plan1.price;
-        const currentDate=new Date();
-        const month=currentDate.getMonth();
-        for(let i=0; i<2; i++){
-            if(plan1.discountMonths[i]==month){
-                bestPrice= plan1.price * .8;
-                //console.log(bestPrice);
-            }
-            else{
-                //console.log("no the current month is not in the plan so u wont get a discount");
-            }
-        }
-    }
-};
-//console.log(plan1);
-plan1.calAnnual();
-function ForPlans(name1,price,space,transfer,pages,discountMonths){
-    // note that the first letter of the constructor is kept for Capital 
-    // it is just for the sake of Understading and easily finnding the constructor seprately from the normal function you can also create a portotype by using the lower case first letter 
-    this.name1=name1;
-    this.price=price;
-    this.space=space;
-    this.transfer=transfer;
-    this.pages=pages;
-    this.discountMonths=discountMonths;
-}
-const plan01=new ForPlans('Basic01',001,00100,"10 GB",10,[2,6,7]);
-const plan02=new ForPlans('pro',002,002,"20 GB",20,[2,6,7]);
-//console.log(plan01);
-//console.log(plan02);
-// now we are going to learn about the browser model
-const whereAt=window.location.href;
-//console.log(whereAt);
-// now we are going to get the domain name of the website
-const DomainName=window.location.hostname;
-//console.log(DomainName);
-const path1=window.location.pathname;
-//console.log(path1);
-const hashing=window.location.hash;
-//console.log(hashing);
-// window.location.href='https://www.facebook.com/';
-// now we are going to learn about the classes in the js 
-class first {
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
+// // again browser object model
+// const ran=window.location;
+// const firstp=ran.href;
+// const secondp=ran.hash;
+// const threep=ran.host;
+// const fourp=ran.pathname;
+// const fivep=ran.port;
+// console.log('port ',fivep);
+// console.log('firstp',firstp);
+// console.log('secondp hash',secondp);
+// console.log('three host',threep);
+// console.log('fourp pathname',fourp);
 
+// now we are going to learn about the exception handling
+// console.log('it should be in the console 1');
+// function playingWithTryCatch1(){
+//     const playingWithTryCatch2='i am an alert with wrong speelings';
+//     alet(playingWithTryCatch2)
+
+// }
+// playingWithTryCatch1();
+// console.log('it should be in the console 2');
+
+
+
+
+
+
+
+
+
+
+
+
+function playingWithTryCatch1(){
+    try{
+        const playingWithTryCatch2='i am an alert with wrong speelings';
+    alet(playingWithTryCatch2)
+    }catch(err){
+       console.log(err);
     }
+
 }
-// we can create the objects by using both classes and the constructor functions
-const first1=new first('zain',20);
-console.log(first);
-console.log(first1);
-import { myself } from "./module1";
-//console.log(myself);
-import Umer from "./module2";
-//console.log("i am umer imported from another modules",Umer);
-import message from "./module1";
-//console.log(message());
-import { JSONTry } from "./module1";
-//console.log(JSONTry);
+playingWithTryCatch1();
+console.log('i am after try catch');
+const val=prompt("enter a value between 5 to 10");
+if(val<5 || val>10){
+    throw('plz enter a valid value');
+}
+
+
+
+
+
+
+
+
